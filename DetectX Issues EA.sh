7c6c3f7939c90a4f2f277451e2fca7bd6b79bafb
ega <everette_allen@ncsu.edu>
2022-01-04 16:08:55 -0500

    Update DetectX Issues EA.sh
    Added test for missing issues data to return string None

--- DetectX Issues EA.sh
+++ DetectX Issues EA.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 
 # Set the path to the results of a DetectX command line scan
- RESULTFILE='/Library/Application Support/JAMF/Addons/DetectX/results.json'
-
+ #RESULTFILE='/Library/Application Support/JAMF/Addons/DetectX/results.json'
+ RESULTFILE='/Volumes/tee/images/projects/Jamf Extension Attributes/Detect X/results.json'
+ 
 # Test to see if the results file exists and has length.  If True use osascript to run javascript
 # and parse our the results if any. Return the date of the scan and the issues list if any
 # or return scan date and the word None. 
@@ -12,7 +13,11 @@ if [ -s $RESULTFILE ]; then
     # run the Javascript
     SDATE=$(/usr/bin/osascript -l JavaScript -e "JSON.parse(\`$json\`).searchdate") 
     ISSUES=$(/usr/bin/osascript -l JavaScript -e "JSON.parse(\`$json\`).issues")
-    ISSUES=$SDATE',\n'$ISSUES
+    if [ -z "$ISSUES" ]; then
+        ISSUES=$SDATE',\nNone'
+    else
+       ISSUES=$SDATE',\n'$ISSUES
+    fi
 else
 # Otherwise return nothing
 ISSUES=""
