c6f8dabc9af6ba956560eac113316de14b03e927
ega <everette_allen@ncsu.edu>
2022-01-04 15:12:26 -0500

    Update DetectX Issues EA.sh
    Simplified the Javascript portions to avoid the read script.

--- DetectX Issues EA.sh
+++ DetectX Issues EA.sh
@@ -6,21 +6,15 @@
 # Test to see if the results file exists and has length.  If True use osascript to run javascript
 # and parse our the results if any. Return the date of the scan and the issues list if any
 # or return scan date and the word None. 
+
 if [ -s $RESULTFILE ]; then
 	json=$(<$RESULTFILE)
-    read -r -d '' JSRUN << EndOfScript
-    function run(){   
-  		var result=JSON.parse(\`$json\`)     
-  		if (result.issues.length > 0){	
-  		return(result.searchdate + "\\n" + result.issues.join('\r\n'))  
-  		}else{
-  		return(result.searchdate + "\\n" + "None")} 
-}   
-EndOfScript
-# run the Javascript we setup
-ISSUES=$(osascript -l JavaScript <<< $JSRUN) 
+    # run the Javascript
+    SDATE=$(/usr/bin/osascript -l JavaScript -e "JSON.parse(\`$json\`).searchdate") 
+    ISSUES=$(/usr/bin/osascript -l JavaScript -e "JSON.parse(\`$json\`).issues")
+    ISSUES=$SDATE',\n'$ISSUES
 else
-# Otherwise return noting
+# Otherwise return nothing
 ISSUES=""
 fi
 
