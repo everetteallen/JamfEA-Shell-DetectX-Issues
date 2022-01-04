59a60f6036425a2d0b480d9607d9c8eb389af7ed
ega <everette_allen@ncsu.edu>
2022-01-04 16:12:01 -0500

    Update DetectX Issues EA.sh
    Remove test results file

--- DetectX Issues EA.sh
+++ DetectX Issues EA.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
 # Set the path to the results of a DetectX command line scan
- #RESULTFILE='/Library/Application Support/JAMF/Addons/DetectX/results.json'
- RESULTFILE='/Volumes/tee/images/projects/Jamf Extension Attributes/Detect X/results.json'
+ RESULTFILE='/Library/Application Support/JAMF/Addons/DetectX/results.json'
+ 
  
 # Test to see if the results file exists and has length.  If True use osascript to run javascript
 # and parse our the results if any. Return the date of the scan and the issues list if any
